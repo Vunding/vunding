4df5e1e0072110ee351b21db218040fe7d593387
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2015-05-23 15:15:34 +0800

    Added login and register form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 $('.progress').progress();
+
+$('.ui.modal').modal();
+
+$('.ui-register').click(function() {
+    $('.ui-register-modal').modal('show');
+    hideAllSections();
+    $('.ui-register-modal-select-section').show();
+    $('.ui-register-modal').modal('show');
+});
+
+$('.ui-login').click(function() {
+    $('.ui-login-modal').modal('show');
+});
+
+function hideAllSections() {
+    $('.ui-register-modal').modal('hide');
+    $('.ui-register-modal-select-section').hide();
+    $('.ui-register-modal-facility-section').hide();
+    $('.ui-register-modal-volunteer-section').hide();
+}
+
+$('.ui-register-modal-facility').click(function() {
+    hideAllSections();
+    $('.ui-register-modal-facility-section').show();
+    $('.ui-register-modal').modal('show');
+});
+
+$('.ui-register-modal-volunteer').click(function() {
+    hideAllSections();
+    $('.ui-register-modal-volunteer-section').show();
+    $('.ui-register-modal').modal('show');
+});
